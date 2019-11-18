56071ac78a4e6d772f85118e28dc63ae56d3c25a
chelseachisholm <chelsea.chisholm@gmail.com>
2019-11-18 17:03:46 +0100

    Flower and abundance performance models added

--- performancemodels.R
+++ performancemodels.R
@@ -23,7 +23,7 @@ cat('model {
     }
 
     #data# Y, X, N
-    #monitor# intercept, coef, mu, tau
+    #monitor# intercept, coef, tau
     }', file='modelfile.txt')
 
 # a named list or a function
@@ -35,7 +35,14 @@ plot(results, 'trace')
 summary(results)			
 
 
-dat <- list(N=N, X=X, Y=Y)
+Y<-as.matrix(dem_data[,c('Density08', 'Density10', 'Density12')])
+dat <- list(N=N, X=X, Y=Y[,1])
+
+
+plot(results)
+
+results			
+
 cat('model {
     intercept ~ dnorm(0, 1/10^6) #remember sd is precision
     coef ~ dnorm(0, 1/10^6)
@@ -44,19 +51,58 @@ cat('model {
     #ntau~ dgamma(10,10)
     #ttau ~ dgamma(10,10)
 
+    #for(t in 1:3){
+    #trand[t] ~ dnorm(0, ttau)
+    #}
+    
+    for(i in 1:N){
+    #rand[i] ~ dnorm(0, ntau)
+        for(t in 1:3) {
+          Y[i,t] ~ dnorm(mu[i,t], tau)
+          log(mu[i,t]) <- intercept + coef*X[i] #+ rand[i] #+ trand[t]
+        }
+    }
+    #data# Y, X, N
+    #monitor# intercept, coef, tau
+    }', file='modelfile.txt')
+
+
+results <- run.jags(model='modelfile.txt', data = dat, burnin=2000, sample=20000, n.chains=2)
+
+plot(results)
+
+coef <- combine.mcmc(results, var='coef')
+intercept <- combine.mcmc(results, var='intercept')
+
+y<- exp(intercept + coef*X)
+
+plot(Y[,3] ~ X)
+
+#### FLOWERS (over time?) ####
+Y<-as.matrix(dem_data[,c('Flower10', 'Flower12')])
+dat <- list(N=N, X=X, Y=Y)
+
+cat('model {
+    intercept ~ dnorm(0, 1/10^6) #remember sd is precision
+    coef ~ dnorm(0, 1/10^6)
+    tau ~ dgamma(10,10)
+    
+    #ntau~ dgamma(10,10)
+    #ttau ~ dgamma(10,10)
+    
     #for(t in 1:(3)){
     #trand[t] ~ dnorm(0, ttau)
     #}
     
     for(i in 1:N){
     #rand ~ dnorm(0, ntau)
-      for(t in 1:3) {
+    for(t in 1:2) {
     Y[i,t] ~ dnorm(mu[i,t], tau)
-    log(mu[i,t]) <- intercept + coef*X[i] #+ rand[i] + trand[t]
+    mu[i,t] <- intercept + coef*X[i] #+ rand[i] + trand[t]
     }
     }
     #data# Y, X, N
-    #monitor# intercept, coef, mu, tau
+    #monitor# intercept, coef, tau
     }', file='modelfile.txt')
 
 
@@ -65,3 +111,42 @@ results <- run.jags(model='modelfile.txt', data = dat, burnin=2000, sample=10000
 plot(results)
 
 results			
+
+coef <- combine.mcmc(results, var='coef')
+intercept <- combine.mcmc(results, var='intercept')
+
+y<- intercept + coef*X
+
+plot(Y[,2] ~ X)
+
+#### EXTRA CODE ####
+
+#Including time as a random effect, but number of observations < 5 so NOT A GOOD IDEA DUE TO MASSIVE SHRINKAGE!!! 
+
+# dat <- list(N=N, X=X, Y=Y)
+# cat('model {
+#     intercept ~ dnorm(0, 1/10^6) #remember sd is precision
+#     coef ~ dnorm(0, 1/10^6)
+#     tau ~ dgamma(10,10)
+#     
+#     #ntau~ dgamma(10,10)
+#     #ttau ~ dgamma(10,10)
+#     
+#     #for(t in 1:(3)){
+#     #trand[t] ~ dnorm(0, ttau)
+#     #}
+#     
+#     for(i in 1:N){
+#     #rand ~ dnorm(0, ntau)
+#     for(t in 1:3) {
+#     Y[i,t] ~ dnorm(mu[i,t], tau)
+#     log(mu[i,t]) <- intercept + coef*X[i] #+ rand[i] + trand[t]
+#     }
+#     }
+#     #data# Y, X, N
+#     #monitor# intercept, coef, tau
+#     }', file='modelfile.txt')
+# 
+# 
+# results <- run.jags(model='modelfile.txt', data = dat, burnin=2000, sample=10000, n.chains=2)
+# 
